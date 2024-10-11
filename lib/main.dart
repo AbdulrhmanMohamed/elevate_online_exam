6ac046fbcff06809743008ffc7cfc88d52608b72
Omar <Omar Abdelgilil007>
2024-10-12 02:55:34 +0300

    input decoration theme

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,21 @@ import 'package:elevate_online_exam/di/di.dart';
 import 'package:elevate_online_exam/presentaion/views/login_screen.dart';
 import 'package:flutter/material.dart';
 
-void main(){
-
-
+void main() {
   runApp(const OnlineExam());
   configureDependencies();
 }
-
 
 class OnlineExam extends StatelessWidget {
   const OnlineExam({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-    
+    return MaterialApp(
+      theme: ThemeData(
+        inputDecorationTheme: const InputDecorationTheme(
+            border: OutlineInputBorder(borderRadius: BorderRadius.zero)),
+      ),
       home: LoginScreen(),
     );
   }

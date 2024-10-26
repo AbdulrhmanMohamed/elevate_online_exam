4fca7608b812c9200cf74e0ce9a52c41fe35a11f
AbdulrhmanMohamed <afarg843@gmail.com>
2024-10-26 17:09:44 +0200

    using Prefs Manager to Save the Token

--- lib/data/api/api_manager.dart
+++ lib/data/api/api_manager.dart
@@ -1,8 +1,10 @@
 import 'package:dio/dio.dart';
+import 'package:elevate_online_exam/common/prefs_manager.dart';
 import 'package:elevate_online_exam/data/api/api_consts.dart';
 import 'package:elevate_online_exam/data/api/models/request/register_body.dart';
 import 'package:elevate_online_exam/data/api/models/response/auth_response/auth_response/auth_response.dart';
 import 'package:elevate_online_exam/data/api/models/response/subject_response/subject_response.dart';
+import 'package:elevate_online_exam/data/consts.dart';
 import 'package:injectable/injectable.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart'; // Add the Pretty Dio Logger
 
@@ -64,9 +66,10 @@ class ApiManager {
     return AuthResponse.fromJson(response.data);
   }
 
-  Future<SubjectResponse?> getSubjects()async{
-  
-   var response=await _dio.get(ApiConsts.getSubjects,options: Options(headers: {"token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3MGQ2YTI4NzA5NjZiNDdkMjk4MjNiMCIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNzI5Njg4NDg5fQ.bcZv9oeS_wuefttLm3cApV4LQcwe62KmJoMKYhf5BwY"}));
-   return SubjectResponse.fromJson(response.data);
+  Future<SubjectResponse?> getSubjects() async {
+    var response = await _dio.get(ApiConsts.getSubjects,
+        options: Options(
+            headers: {"token": CacheHelper.getData(key: Consts.token)}));
+    return SubjectResponse.fromJson(response.data);
   }
 }
---
+++ lib/data/consts.dart
@@ -0,0 +1,5 @@
+class Consts{
+
+static String token="token";
+
+}
